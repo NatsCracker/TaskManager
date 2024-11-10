7d0a0f42cd218659e741af5b39be424e3ab387ae
Nikita_Chermakov <nikita@chermakov.ru>
2024-11-10 11:16:28 +0300

    v1.121

--- src/Epic.java
+++ src/Epic.java
@@ -21,36 +21,6 @@ public class Epic extends Task {
         subtasksId.remove(Integer.valueOf(subtaskId));
     }
 
-    // Метод обновления статуса эпика на основе статусов его подзадач
-    public void updateEpicStatus(HashMap<Integer, Subtask> subtasks) {
-        if (subtasksId.isEmpty()) {
-            setStatus(TaskStatus.NEW);
-            return;
-        }
-
-        boolean allDone = true;
-        boolean anyInProgress = false;
-
-        for (Integer subtaskId : subtasksId) {
-            Subtask subtask = subtasks.get(subtaskId);
-            if (subtask != null) {
-                if (subtask.getStatus() == TaskStatus.IN_PROGRESS) {
-                    anyInProgress = true;
-                }
-                if (subtask.getStatus() != TaskStatus.DONE) {
-                    allDone = false;
-                }
-            }
-        }
-
-        if (allDone) {
-            setStatus(TaskStatus.DONE);
-        } else if (anyInProgress) {
-            setStatus(TaskStatus.IN_PROGRESS);
-        } else {
-            setStatus(TaskStatus.NEW);
-        }
-    }
 
     @Override
     public String toString() {
