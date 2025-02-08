e5fb18c0486e3eaa6ab357c80ba7022ee4d8b937
Nikita_Chermakov <nikita@chermakov.ru>
2025-02-08 17:36:07 +0300

    V3.7
    - Добавлено время старта, окончания и продолжительность всех типов задач
    - Добавлена приоритизация задач
    - Добавлена проверка на пересение по времени между задачами
    - Циклы заменены на стримы и там где можно было

--- src/main/service/FileBackedTaskManager.java
+++ src/main/service/FileBackedTaskManager.java
@@ -143,6 +143,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     // Метод для преобразования из строки в задачу
     private static Task fromString(String value) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         String[] parts = value.split(",");
         int id = Integer.parseInt(parts[0]);
         TaskType type = TaskType.valueOf(parts[1]);
@@ -161,6 +162,14 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         int id = Integer.parseInt(parts[0]);
         TaskType type = TaskType.valueOf(parts[1]);
 >>>>>>> 1cac1a9 (V3.7)
+=======
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        String[] parts = value.split(",");
+        int id = Integer.parseInt(parts[0]);
+        TaskType type = TaskType.valueOf(parts[1]);
+>>>>>>> 84824b1 (V3.7)
         switch (type) {
             case TASK:
                 Task task = new Task(parts[2], parts[4]);
@@ -168,6 +177,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
                 task.setStatus(TaskStatus.valueOf(parts[3]));
                 return task;
             case SUBTASK:
+<<<<<<< HEAD
 <<<<<<< HEAD
                 int epicId = Integer.parseInt(parts[7]);
                 task = new Subtask(name, description, epicId, duration, startTime);
@@ -192,6 +202,21 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         }
         
         return null; // Placeholder return, actual implementation needed
+=======
+                Subtask subtask = new Subtask(parts[2], parts[4], 
+                        Integer.parseInt(parts[5]));
+                subtask.setId(id);
+                subtask.setStatus(TaskStatus.valueOf(parts[3]));
+                return subtask;
+            case EPIC:
+                Epic epic = new Epic(parts[2], parts[4]);
+                epic.setId(id);
+                epic.setStatus(TaskStatus.valueOf(parts[3]));
+                return epic;
+            default:
+                throw new IllegalArgumentException("Неизвестный тип задачи: " + type);
+        }
+>>>>>>> 84824b1 (V3.7)
     }
 
     // Метод для создания задачи
