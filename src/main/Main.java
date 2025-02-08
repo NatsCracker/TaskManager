8874af0d759ddc737622ab6effa23f18da72e6f3
Nikita_Chermakov <nikita@chermakov.ru>
2025-02-08 17:36:07 +0300

    V3.7
    - Добавлено время старта, окончания и продолжительность всех типов задач
    - Добавлена приоритизация задач
    - Добавлена проверка на пересение по времени между задачами
    - Циклы заменены на стримы и там где можно было

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,7 @@
 package main;
 
-import main.model.Epic;
-import main.model.Subtask;
-import main.model.Task;
-import main.service.TaskManager;
-import main.util.Managers;
-
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Трекер задач запущен!");
     }
 }
